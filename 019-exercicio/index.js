9d97368631e23303858114408e4aaa1daaa902b8
estebantiago <168283080+estebantiago@users.noreply.github.com>
2025-08-30 17:01:21 -0300

    adicionando exercicios

---
+++ 019-exercicio/index.js
@@ -0,0 +1,8 @@
+//  Faça um algoritmo que imprima na tela a tabuada de 1 até 10.
+
+for(let i = 1; i <= 10; i++){
+    for(let j = 1; j <= 10; j++ ){
+        console.log(`${i} x ${j} = ${i * j }`);
+    }
+    console.log('=================')
+}
---
+++ 020-exercicio/index.js
@@ -0,0 +1,8 @@
+// Faça um algoritmo que receba um valor inteiro e imprima na tela a sua tabuada.
+
+
+let numero = 5;
+
+for(let i = 1; i <= 10; i++){
+   console.log(`${numero} x ${i} = ${numero * i}`);
+}
