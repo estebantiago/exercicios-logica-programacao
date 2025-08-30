be6c03e7ded591ab960606cde280c12776bb913b
estebantiago <168283080+estebantiago@users.noreply.github.com>
2025-08-30 08:07:14 -0300

    Create index.js

---
+++ 014-exercicio/index.js
@@ -0,0 +1,22 @@
+// Faça um algoritmo que receba um valor A e B, e troque o valor de A por B e o valor de B por A e imprima na tela os valores.
+let A = 10;
+let B = 2;
+
+console.log(`Valor Original:  A = ${A} e Valor de B = ${B}`);
+
+
+let guardandoValor = A;
+
+ A = B;
+ B = guardandoValor;
+
+ console.log(`Valor trocado : A = ${A} B = ${B}`);
+
+
+ 
+
+
+
+
+
+
