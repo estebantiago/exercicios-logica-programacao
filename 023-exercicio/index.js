a5f420593dd1430f7a85cd968c3a6d0a94409575
estebantiago <168283080+estebantiago@users.noreply.github.com>
2025-08-31 21:29:26 -0300

    Update index.js

--- 023-exercicio/index.js
+++ 023-exercicio/index.js
@@ -1,2 +1,13 @@
 // Faça um algoritmo que efetue o cálculo do salário líquido de um professor. As informações fornecidas serão: valor da hora aula, número de aulas lecionadas no mês e percentual de desconto do INSS. Imprima na tela o salário líquido final.
 
+
+function calcularSalarioLiquido (valorHoraAula, qtdAulasMensais, percentualDesconto){
+    let desconto = percentualDesconto / 100;
+    let calculo = valorHoraAula * qtdAulasMensais;
+    let resultado = calculo * desconto;
+    return calculo - resultado;
+
+}
+let valorFinal = calcularSalarioLiquido(100, 40, 9);
+
+console.log(`Salário Líquido Final: ${valorFinal}`);
